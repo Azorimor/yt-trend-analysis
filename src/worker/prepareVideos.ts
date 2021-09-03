e371a6f78a57be74dd8e42920aed0a8db3f62498
Azorimor <azorimor@gmail.com>
2021-09-03 22:03:39 +0200

    Fix calculation of job progress

--- src/worker/prepareVideos.ts
+++ src/worker/prepareVideos.ts
@@ -8,7 +8,7 @@ export const prepareVideosWorker = new Worker(
     console.log(`Job executed: ${job.id}`);
     REGIONS.forEach((region, i) => {
       trendingVideosQueue.add('load-region', {region: region});
-      job.updateProgress((i + 1) / REGIONS.length);
+      job.updateProgress(((i + 1) / REGIONS.length) * 100);
     });
   },
   {connection: REDIS_CONNECTION}
