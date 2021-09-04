beaa15bfcba462057b1afbfb5e31beb1d267e053
Azorimor <azorimor@gmail.com>
2021-09-04 22:19:55 +0200

    added mongo storage to loaded videos

--- src/worker/loadVideo.ts
+++ src/worker/loadVideo.ts
@@ -1,14 +1,18 @@
 import {Worker, Job} from 'bullmq';
 import {REDIS_CONNECTION} from '../utils/config';
 import {YoutubeService} from '../services/youtube';
+import {VideoModel} from '../models/video';
 
 const youtube = new YoutubeService();
 
 export const loadVideosWorker = new Worker(
   'load-trending-videos',
   async (job: Job) => {
-    console.log(`Region: ${job.data.region}`);
-    youtube.getTrendingVideos(job.data.region);
+    const videos = await youtube.getTrendingVideos(job.data.region);
+    job.updateProgress(50);
+    await VideoModel.insertMany(videos);
+    job.updateProgress(100);
+    return videos.map(video => video.snippet);
   },
   {connection: REDIS_CONNECTION}
 );
