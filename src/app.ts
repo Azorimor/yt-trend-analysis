0d32eb485bbc1d5ff6fac80913448977f71e9ecf
Azorimor <azorimor@gmail.com>
2021-09-05 00:20:22 +0200

    Added ejs and login page

--- src/app.ts
+++ src/app.ts
@@ -9,4 +9,11 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.set('views', __dirname + '/../views');
+app.set('view engine', 'ejs');
+
+app.get('/login', (req: express.Request, res: express.Response) => {
+  res.render('login');
+});
+
 export default app;
